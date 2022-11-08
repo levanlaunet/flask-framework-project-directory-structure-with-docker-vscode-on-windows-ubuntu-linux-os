880066f92bf869a086f83d2c84d13d77e3af9fc4
levanlau <levanlau.net@gmail.com>
2022-11-08 21:00:29 +0700

    Update something

--- app/apps/app1/views.py
+++ app/apps/app1/views.py
@@ -1,10 +1,10 @@
-# from flask import Blueprint, request, url_for, redirect, render_template, flash
+# from flask import Blueprint, render_template
 # app1 = Blueprint('app1', __name__, template_folder="templates/app1")
 
 # @app1.route("/")
 # def home():
 #     return render_template('index.html')
 
-# @app1.app_errorhandler(404)
+# @app1.errorhandler(404)
 # def page_not_found(e):
 #     return render_template('404.html'), 404
